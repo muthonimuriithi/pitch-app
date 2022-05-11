069fe1a6083578801514b54817eb276d4e05ef51
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-05-11 08:44:10 +0300

    added run commands

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
-export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://MURIITHI:muthoni@localhost:5432/pitches'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://muriithi:muthoni@localhost/pitches'
 
 # python3 run.py shell
+
+python3 run.py server
+
+
+# python run.py db init
+# python run.py db migrate -m "Initial Migration"
+# python run.py db upgrade
