6fa2aaf0ad2bdc8483a8559be9d82d17773e734e
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-05-11 08:05:52 +0300

    added import loginmaster from flask

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,22 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .config import config_options
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+
+
 
 bootstrap = Bootstrap()
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app (environment):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(config_options[environment])
     db.init_app(app)
+    login_manager.init_app(app)
     from .main import auth
     app.register_blueprint(auth)
 
