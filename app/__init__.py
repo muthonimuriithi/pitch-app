c6dcce27b3cb32a1e9fa21c2036e75aebb45e3e3
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-05-13 00:05:10 +0300

    ft:registered main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from .config import config_options
+from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 
@@ -14,11 +14,13 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
 def create_app (environment):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
     app.config.from_object(config_options[environment])
     db.init_app(app)
     login_manager.init_app(app)
-    from .main import auth
-    app.register_blueprint(auth)
+    # from .auth import auth as auth_blueprint
+    # app.register_blueprint(auth_blueprint)
 
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
     return app
