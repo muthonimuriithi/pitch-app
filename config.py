ae720ff4896f212f09332417e3a61332c84540ed
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-05-15 05:47:36 +0300

    added uri to devprod

---
+++ config.py
@@ -0,0 +1,37 @@
+import os
+class Config:
+    
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+        #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+class ProdConfig(Config):
+
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://muriithi:muthoni@localhost/pitches'
+    
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://muriithi:muthoni@localhost/pitches'
+
+    DEBUG= True
+    ENV = 'development'
+
+config_options = {
+    'production': ProdConfig,
+    'development': DevConfig
+}
